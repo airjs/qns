f4eaa92e82d3a7a593feb4dbbf407b1146b72c6b
nighca <nighca@live.cn>
2013-10-17 19:15:14 +0800

    update test

--- test/cache/index.js
+++ test/cache/index.js
@@ -1,6 +1,11 @@
 /**
  * 测试cache get, set, expires
  *
+* use:
+ *      $mocha --timeout 20000 test/cache/redis.js
+ *      * 20000 could be some other value depends on testCases.expires
+ * otherwise mocha will throw:
+ *      Error: timeout of 2000ms exceeded
  */
 
 var should = require('should');
@@ -12,25 +17,6 @@ var apis = ['set', 'get', 'isAboutToExpired'];
 // only formatted vals could be cached
 var format = function(obj){
     return JSON.parse(JSON.stringify(obj));
-};
-
-var like = function (target, source) {
-    var type = typeof source;
-
-    if(type !== typeof target){
-        return false;
-    }
-
-    if(type !== 'object'){
-        return target === source;
-    }
-
-    for (var p in source) {
-        if (!like(target[p], source[p])) {
-            return false;
-        }
-    }
-    return true;
 };
 
 var testCases = [
@@ -46,16 +32,17 @@ var testCases = [
         val: {
             num: 1
         },
-        expires: 2
+        expires: 3
     },
     {
         key: 'c',
         val: {
             son: {
                 name: 'who?'
-            }
+            },
+            say: function(){}
         },
-        expires: 13
+        expires: 11
     }
 ];
 
@@ -63,31 +50,45 @@ describe('Cache', function(){
     var cache = new Cache();
 
     describe('#new', function(){
+
         it('should return a cache object with right apis', function () {
             cache.should.have.properties(apis);
         });
+
     });
 
     testCases.forEach(function(testCase, i){
-        cache.set(testCase.key, testCase.val, {
-            expires: testCase.expires
-        });
-    });
 
-    describe('#set(), #get()', function(done){
-        it('should set successfully & get right value', function(){
-            testCases.forEach(function(testCase, i){
+        describe('#set(), #get() ' + i, function(){
+            cache.set(testCase.key, testCase.val, {
+                expires: testCase.expires
+            });
+            testCase.expireAt = Date.now() + testCase.expires * 1000;
+
+            it('should set successfully & get right value', function(){
                 var a = format(testCase.val),
-                    b = cache.get(testCase.key),
-                    right;
-                switch(typeof a){
-                case 'object':
-                    right = like(a, b);
-                    break;
-                default:
-                    right = a === b;
-                }
-                right.should.equal(true);
+                    b = cache.get(testCase.key);
+                a.should.eql(b);
+            });
+        });
+
+        describe('#isAboutToExpired ' + i, function(){
+            it('should get right value now', function(done){
+                var t = Date.now();
+                var isAboutToExpired = t + 10000 > testCase.expireAt && t < testCase.expireAt;
+                cache.isAboutToExpired(testCase.key).should.equal(isAboutToExpired);
+                done();
+            });
+        });
+
+        describe('#isAboutToExpired ' + i, function(){
+            it('should get right value 2s later', function(done){
+                setTimeout(function(){
+                    var t = Date.now();
+                    var isAboutToExpired = t + 10000 > testCase.expireAt && t < testCase.expireAt;
+                    cache.isAboutToExpired(testCase.key).should.equal(isAboutToExpired);
+                    done();
+                }, 2000);
             });
         });
     });
